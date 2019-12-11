cd3ae0bfbe3b771da288b677cc0fe4e211f8effc
Dawid Kowalski <dawid.kowalski@vw-poznan.pl>
2019-12-11 15:23:24 +0100

    images.js

---
+++ script/images.js
@@ -0,0 +1,17 @@
+const images = [
+    "https://picsum.photos/id/40/300/150",
+    "https://picsum.photos/id/90/300/150",
+    "https://picsum.photos/id/56/300/150"
+    
+];
+
+
+// const image1 = 'https://picsum.photos/id/40/300/150';
+// const image2 = 'https://picsum.photos/id/90/300/150';
+// const image3 = 'https://picsum.photos/id/56/300/150';
+
+// const images = [image1, image2, image3];
+
+//images.push(image1);
+//images.push(image2);
+//images.push(image3);
--- script/main.js
+++ script/main.js
@@ -1,43 +1,29 @@
-const image1 = 'https://picsum.photos/id/40/300/150';
-const image2 = 'https://picsum.photos/id/90/300/150';
-const image3 = 'https://picsum.photos/id/56/300/150';
+// for (const image of images) {
+//     console.log("to jest obrazek" + image);
+//     console.log('PK');
+// }
 
-const images = [image1, image2, image3];
+// //i++  i = i+1
+// //pętla for
+// for (let i = 0; i < images.length; i++) {
+//     const image = images[i];
+//     console.log("to jest obrazek" + image);
+//     console.log('PK')
+// }
 
-//images.push(image1);
-//images.push(image2);
-//images.push(image3);
-
-
- for (const image of images) {
- console.log("to jest obrazek"+ image); 
- console.log('PK'); 
- }
-
- //i++  i = i+1
- //pętla for
- for (let i = 0; i < images.length; i++){
-     const image = images[i];
-     console.log("to jest obrazek"+ image);
-     console.log('PK')
- }
-
- const isEmpty = (images.length===0);
- const isEmpty1 = (images.length==0);
+const isEmpty = (images.length === 0);
+const isEmpty1 = (images.length == 0);
 
 
- //definicja funkcji, mozna ja ja wywolac kiedy sie chce
- function displayImages() {
+//definicja funkcji, mozna ja wywolac kiedy sie chce
+function displayImages() {
     console.log('liczba obrazków' + images.length);
     console.log('pierwszy element listy' + images[0]);
     console.log('drugi element listy' + images[1]);
 }
 
-//wywolanie funkcji
-displayImages();
-
-function displayLastElement (){
-    console.log('ostatni element z listy' + images[images.length-1]);
+function displayLastElement() {
+    console.log('ostatni element z listy' + images[images.length - 1]);
     console.log(43455234 / 1242);
     console.log(43455234 * 1242); //asterix
     console.log(43455234 + 1242);
@@ -45,14 +31,14 @@ function displayLastElement (){
 }
 
 
-function renderPhoto(){
-    const url='https://picsum.photos/300/150';
-    const img= document.createElement('img');
+function renderPhoto() {
+    const url = 'https://picsum.photos/300/150';
+    const img = document.createElement('img');
     img.src = url;
 
     const main = document.querySelector('main');
     main.append(img);
-    
+
 }
 //
 // function renderPhoto(){
@@ -63,20 +49,21 @@ function renderPhoto(){
 //     const main = document.querySelector('main');
 //     main.append(img);
 
-function renderPhoto(url){
-    const img= document.createElement('img');//tworzenie obrazu , brak widoczności
+function renderPhoto(url) {
+    const img = document.createElement('img');//tworzenie obrazu , brak widoczności
     img.src = url;
     const main = document.querySelector('main');
     main.append(img);//wyswietlenie utworzonego obrazka do wyświetlenia
 }
 
-function displayPhoto(photos){
-    console.log(photos);
-    for (const photo of photos){
-    renderPhoto(photo);
+function displayPhotos(images) {
+    console.log(images);
+    for (const image of images) {
+        renderPhoto(image);
     }
 }
-displayPhoto(images);
+
+displayPhotos(images);
 
 //  renderPhoto('http://picsum.photos/id/10/300/150');
 //  renderPhoto('http://picsum.photos/id/20/300/150');
