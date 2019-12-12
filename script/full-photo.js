e718fa7b980a2c2ba1fcd01c84e7514274d32b1a
Dawid Kowalski <dawid.kowalski@vw-poznan.pl>
2019-12-12 14:37:39 +0100

    po bluescreen

---
+++ script/full-photo.js
@@ -0,0 +1,15 @@
+
+class FullPhoto{
+
+    size = {
+        width:1920,
+        height: 1080,
+    } ;
+    // Metoda -> Method
+    display (){}
+    close () {}    
+}
+const big1 = new FullPhoto();
+const big2 = new FullPhoto();
+
+
